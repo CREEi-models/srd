05b0e45120b0e25b2f596d9e0a3d2cd87bdc1f6c
Pierre-Carl <pcmichaud@gmail.com>
2020-04-19 16:13:46 -0400

    removing cpp as a submodule

--- srd/__init__.py
+++ srd/__init__.py
@@ -3,4 +3,3 @@ from .tools import get_params, add_params_as_attr, add_schedule_as_attr
 from .actors import Person, Dependent, Hhold
 from .payroll import payroll
 from .calculators import tax
-from .cpp import cpp
