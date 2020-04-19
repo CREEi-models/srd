2785dcf6018430880dc21261900470a45ef4b618
Pierre-Carl <pcmichaud@gmail.com>
2020-04-19 16:46:16 -0400

    not sure

--- srd/payroll.py
+++ srd/payroll.py
@@ -1,7 +1,7 @@
 from srd import add_params_as_attr
 import os
 module_dir = os.path.dirname(os.path.dirname(__file__))
-from cpp import cpp 
+from srd.cpp import cpp 
 from srd import qpip 
 from srd import ei
 import numpy as np
