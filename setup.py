a4cc4b666aa510f97417abefe1b6b38dcb153d34
Pierre-Yves Yanni <pyanni@gmail.com>
2020-08-14 11:18:15 -0400

    change to doc simulateur Canada

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="srd", # Replace with your own username
+    name="srd",  # Replace with your own username
     version="1.1.0",
     author="Equipe CREEi",
     author_email="pierre-carl.michaud@hec.ca",
--- upload_to_pypi.bat
+++ upload_to_pypi.bat
@@ -1,4 +1,4 @@
-:: pip install build
-rm dist/*
+:: this does not work on dropbox; clone somewhere else before executing this file
+del /q dist
 python setup.py sdist bdist_wheel 
 twine upload --repository pypi dist/* --verbose
